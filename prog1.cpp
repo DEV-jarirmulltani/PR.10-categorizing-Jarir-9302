b219da3fd58a480431dcfdc9f4aeea15886d6b22
dev-jarir08 <your-email-address>
2025-06-18 09:36:01 +0530

    second

--- prog1.cpp
+++ prog1.cpp
@@ -28,16 +28,60 @@ class sorting
     int Merge(vector<int>&arr , int start , int mid , int end)
     {
       vector<int>temp;
-      int i =start , j = mid + 1;
-      while (i<=mid && j<= end)
-      {
-        /* code */
-      }
-      
 
+      int i =start , j = mid + 1;
+
+      while (i<=mid && j <= end)
+      {
+        if (arr[i]<= arr[j])
+        {
+            temp.push_back (arr[i]);
+        }
+        else
+        {
+            temp.push_back (arr[j]);
+        }
+      }
+
+
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
     }
-    int linear_serch(){}
-    int binary_serch(){}
+
+    while (j <= end)
+    {
+        temp.push_back(arr[j]);
+    }
+
+    for (int k = 0; k < temp.size(); k++)
+    {
+        arr.at(start + k) = temp.at(k);
+    }
+
+}
+    int Merge_sort(vector<int>&arr,int start ,  int end)
+    {
+        if (start>= end)
+        return 0 ;
+          int mid = start + (end - start) / 2;
+
+    Merge_sort(arr, start, mid);
+    Merge_sort(arr, mid + 1, end);
+    Merge(arr, start, mid, end);
+}
+
+int LinearSearch(vector<int> &array,int target)
+{
+    for (int i = 0; i < array.size(); i++)
+    {
+        if (arr.at(i) == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 };
 
 
